7268ff9d72f71890e18817179e1820ce127f7fc4
iyannik0215 <iyannik0215@gmail.com>
2016-09-30 13:33:07 +0800

    update.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -r ~/Volumes/ghost/themes/my-casper/* ~/Gitwork/my-casper
+cp -r ~/Volumes/ghost/themes/my-casper/* ~/Gits/my-casper
 git add --all
 git commit -am "update."
 git push -u origin master
